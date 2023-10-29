d9dd0e576335edee84ceb4aa61e2ca2d99c44b6b
Abstaina44 <padcliff88@gmail.com>
2023-10-29 07:29:50 +0000

    Amended an index file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,28 +1,46 @@
 #!/usr/bin/python3
-"""index.py"""
-from models import storage
-from api.v1.views import app_views
+"""
+index
+"""
+
 from flask import jsonify
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+from api.v1.views import app_views
+
+from models import storage
 
 
-@app_views.route("/status", strict_slashes=False)
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
 def status():
-    """Returns status"""
-    return jsonify({"status": "OK"})
+    """
+    status route
+    :return: response with json
+    """
+    data = {
+        "status": "OK"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
 
 
-@app_views.route("/stats")
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
 def stats():
-    """counts and returns each table values"""
-    classes = {"amenities": Amenity, "cities": City, "places": Place,
-               "reviews": Review, "states": State, "users": User}
-    stats_dict = {}
-    for key, value in classes.items():
-        stats_dict[key] = storage.count(value)
-    return jsonify(stats_dict)
+    """
+    stats of all objs route
+    :return: json of all objs
+    """
+    data = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
